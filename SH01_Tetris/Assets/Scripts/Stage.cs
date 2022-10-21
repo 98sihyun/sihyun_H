774666c0f2cd8cd5ddfb68751703f426eed2ff0b
98sihyun <zotlvmf@naver.com>
2022-10-21 15:11:50 +0900

    01_테트리스- Stage 작성중 (행 노드 자식추가)

--- SH01_Tetris/Assets/Scripts/Stage.cs
+++ SH01_Tetris/Assets/Scripts/Stage.cs
@@ -34,6 +34,14 @@ public class Stage : MonoBehaviour
         nextFallTime = Time.time + fallCyecle;      // fallCyecle을 사용해 다음에 떨어질 시간을 지정
 
         CreateBackground();
+
+        for (int i =0; i <boardHeight; ++i)
+        {
+            var col = new GameObject((boardHeight - i - 1).ToString()); // ToString()은 c#기본기능, 정수를 문자열로 변환해 주는 메서드
+            col.transform.position = new Vector3(0, halfHeight- i, 0);  // 각 노드는 이름의 위치의 가로 중앙에 위치한다.
+            col.transform.parent = boardNode;                           // 보드 노드의 자식으로 만들어준다
+        }
+
         CreateTetromino();
     }
 
@@ -46,25 +54,28 @@ public class Stage : MonoBehaviour
         {
             moveDir.x = -1;
         }
+
         else if(Input.GetKeyDown(KeyCode.RightArrow))
         {
-            moveDir.x = -1;
+            moveDir.x = 1;
         }
+
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             isRotate = true;
         }
+
         else if( Input.GetKeyDown(KeyCode.DownArrow))
         {
             moveDir.y = -1;
         }
+
         //방향벡터가 0이고 회전이 없다면 입력값이 없다는 의미이므로 그렇지 않은 경우에만 테트로미노를 이동하는 메서드를 호출
         if (moveDir != Vector3.zero || isRotate)
         {
             MoveTetromino(moveDir, isRotate);
         }
 
-        
         if(Time.time > nextFallTime)
         { // 현재시간이 다음 떨어질 시간보다 크면 강제로 이동시키고 , 사용자 입력을 무시
 
@@ -86,12 +97,77 @@ public class Stage : MonoBehaviour
     /// <returns>방향과 회전을 인자로 받으며 이동이 가능하면 true, 이동이 불가능하면 false를 반환
     bool MoveTetromino(Vector3 moveDir, bool isRotate)
     {
+        Vector3 oldPos = tetrominoNode.transform.position;      // 이동 가능한지 확인 후 불가능하면 
+        Quaternion oldRot = tetrominoNode.transform.rotation;   // 원래 위치로 되돌리기 위해 현재 값을 저장
+
         tetrominoNode.transform.position += moveDir;
         if (isRotate)
         {
             tetrominoNode.transform.rotation *= Quaternion.Euler(0, 0, 90); // z 기준으로 회전
         }
 
+        if(!CanMoveTo(tetrominoNode))   // 반환값이 거짓(false)이면 이동이 불가능, 위에서 저장해둔 기존 위치로 되돌아간다.
+        {
+            tetrominoNode.transform.position = oldPos;
+            tetrominoNode.transform.rotation = oldRot;
+
+            if((int)moveDir.y == -1 && (int)moveDir.x == 0&& isRotate ==false)
+            {
+                AddToBoard(tetrominoNode);
+                CheckBoardColumn();
+                CreateTetromino();
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private void AddToBoard(Transform root)
+    {
+        while (root.childCount > 0)     // 테트로미노의 모든 자식 타일을 가져오는 코드
+        {
+           var node = root.GetChild(0); // childCount가 바뀐다는 뜻으로 모든 자식을 이동시킬 수 없음.
+            int x = Mathf.RoundToInt(node.transform.position.x + halfWidth);        // 좌표
+            int y = Mathf.RoundToInt(node.transform.position.y + halfHeight - 1);   // 변경
+
+            node.parent = boardNode.Find(y.ToString()); // y 좌표로 보드 노드에 있는 행 노드를 찾아와서 그 자식으로 만들어 줍니다.
+            node.name =x.ToString();                    
+        }
+    }
+
+    private void CheckBoardColumn()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    ///  이동 가능한지 체크 ,true 가능 / false 불가능
+    /// </summary>
+    /// <param name="root">root에 붙어 있는 i번째 자식을 가져오는 것</param>
+    /// <returns></returns>
+    bool CanMoveTo(Transform root)
+    {
+        for(int i = 0; i< root.childCount; ++i) // 자식들에 붙어 있는 모든 타일들
+        {
+            var node = root.GetChild(i);    // var는 Transform과 같은 의미
+            int x = Mathf.RoundToInt(node.transform.position.x + halfWidth);
+            int y = Mathf.RoundToInt(node.transform.position.y + halfHeight - 1);
+
+            if (x < 0 || x > boardWidth - 1)
+         //변환한 좌표가 안전한 위치인지 확인하는 코드입니다. 0 ~ boardWidth -1까지가 안전한 위치이고 그렇지 않으면 이동이 불가능한 영역
+                return false;
+
+            if (y < 0)
+         //기본적으로 테트로미노는 아래로만 이동이 가능하므로 아래쪽 0 보다 작으면 이동이 불가능한 영역
+                return false;
+
+            var column = boardNode.Find(y.ToString());  // 현재 y위치에 해당하는 행 노드를 가져온다.
+
+            if (column != null && column.Find(x.ToString()) != null) // 추가된 타일의 이름은 x 값으로 만들어질예정
+                return false;
+        }
         return true;
     }
 
